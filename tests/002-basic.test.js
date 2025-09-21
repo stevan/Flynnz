cf1cb4825fd5483745ecf4834d00c021715e7519
Stevan Little <stevan.little@gmail.com>
2025-09-21 15:16:32 +0200

    got pop working nicely, and simplified the instructions

--- tests/002-basic.test.js
+++ tests/002-basic.test.js
@@ -64,32 +64,35 @@ const HALT = 'HALT';
 // -----------------------------------------------------------------------------
 
 let powersOfTwo = [
-    [ SCAN, PUSH,   2,  1,  1, FALSE ],
-    [ SCAN, DUP,  ___,  1,  1, FALSE ],
-    [ SCAN, ADD,  ___,  1,  1, FALSE ],
-    [ JUMP, ANY,  ___, -2,  0, FALSE ],
-    [ HALT, ___,  ___,  0,  0, FALSE ],
+    [ SCAN, PUSH,   2,  1, TRUE  ],
+    [ SCAN, DUP,  ___,  1, FALSE ],
+    [ SCAN, ADD,  ___,  1, FALSE ],
+    [ SCAN, DUP,  ___,  1, TRUE  ],
+    [ SCAN, PUSH, 256,  1, FALSE ],
+    [ SCAN, EQ,   ___,  1, FALSE ],
+    [ JUMP, EQZ,  ___, -5, FALSE ],
+    [ HALT, ___,  ___,  0, FALSE ],
 ];
 
 let countdown = [
-    [ SCAN,  PUSH,  10,  1,  1, TRUE,   ],
-    [ SCAN,  DUP,  ___,  1,  1, FALSE,  ],
-    [ SCAN,  PUSH,   1,  1,  1, FALSE,  ],
-    [ SCAN,  SUB,  ___,  1,  1, FALSE,  ],
-    [ SCAN,  DUP,  ___,  1,  1, TRUE,   ],
-    [ SCAN,  PUSH,   1,  1,  1, FALSE,  ],
-    [ SCAN,  EQ,   ___,  1,  1, FALSE,  ],
-    [ JUMP,  EQZ,  ___, -6, -3, FALSE,  ],
-    [ HALT,  ___,  ___,  0,  0, FALSE,  ],
+    [ SCAN,  PUSH,  10,  1, TRUE,   ],
+    [ SCAN,  DUP,  ___,  1, FALSE,  ],
+    [ SCAN,  PUSH,   1,  1, FALSE,  ],
+    [ SCAN,  SUB,  ___,  1, FALSE,  ],
+    [ SCAN,  DUP,  ___,  1, TRUE,   ],
+    [ SCAN,  PUSH,   1,  1, FALSE,  ],
+    [ SCAN,  EQ,   ___,  1, FALSE,  ],
+    [ JUMP,  EQZ,  ___, -6, FALSE,  ],
+    [ HALT,  ___,  ___,  0, FALSE,  ],
 ];
 
-let test = [
-    [ SCAN, PUSH,   2,  1,  1, TRUE ],
-    [ SCAN, DUP,  ___,  1,  1, TRUE ],
-    [ SCAN, PUSH,   5,  1,  1, TRUE ],
-    [ SCAN, POP,  ___,  1,  1, TRUE ],
-    [ SCAN, ADD,  ___,  1,  1, TRUE ],
-    [ HALT, ___,  ___,  0,  0, TRUE ],
+let popTest = [ // result should be 8
+    [ SCAN, PUSH,   5,  1, FALSE ],
+    [ SCAN, PUSH,   3,  1, FALSE ],
+    [ SCAN, PUSH,  10,  1, FALSE ],
+    [ SCAN, POP,  ___,  1, FALSE ],
+    [ SCAN, ADD,  ___,  1, TRUE  ],
+    [ HALT, ___,  ___,  0, FALSE ],
 ];
 
 // -----------------------------------------------------------------------------
@@ -106,17 +109,18 @@ let tos   = -1;
 let output = [];
 
 // load the program
-let program = test;
+let program = powersOfTwo; // popTest; //countdown;
 
 // execute until we hit the end, or an error
 while (state != HALT && state != ERR) {
-    // allocate a temp ...
+    // loop local variables
     let temp;
+    let tosm = 1;
 
     // -------------------------------------------------------------------------
     // Decode the instruction
     // -------------------------------------------------------------------------
-    let [ st, op, data, tm, tosm, retain ] = program[ip];
+    let [ st, op, data, tm, retain ] = program[ip];
 
     //console.log('INSTRUCTION', program[ip]);
 
@@ -144,7 +148,7 @@ while (state != HALT && state != ERR) {
             // negative numbers are only allowed
             // for jumps as a means of carrying
             // over the values to the next loop
-            temp = output[tos + tosm][0];
+            temp = output[tos - 3][0];
             break;
         default:
             // if we don't know the op, then we should halt and complain!
@@ -165,12 +169,23 @@ while (state != HALT && state != ERR) {
         // ----------------------------------------------
         // PUSH (      -- n     )
         // DUP  (    n -- n n   )
+        // POP  (    n --       )
         case PUSH:
             temp = data;
             break;
         case DUP:
             // simply duplicate the previous stack value
             temp = output[tos][0];
+            break;
+        // ----------------------------------------------
+        // Pop is a little funny because it basically
+        // just tells the next operation to ignore the
+        // top of stack, which I think will work for
+        // most situations, but I am not 100% sure.
+        // ----------------------------------------------
+        case POP:
+            // the TOS movement should DWIM
+            tosm = -1;
             break;
         // ----------------------------------------------
         // FIXME:
@@ -180,15 +195,10 @@ while (state != HALT && state != ERR) {
         // to do within the log, ... not sure how to do
         // it yet, so we leave this here.
         // ----------------------------------------------
-        // POP  (     n --       )
         // SWAP (   a b -- b a   )
         // ROT  ( a b c -- c b a )
         // ----------------------------------------------
-        case POP:
-        case SWAP:
-        case ROT:
-            throw new Error(`TODO - ${op}`);
-            break;
+
         // ----------------------------------------------
         // maths ...
         // ----------------------------------------------
@@ -262,7 +272,7 @@ while (state != HALT && state != ERR) {
     state = st;
     pc   += 1;
     ip   += tm;
-    tos  += Math.max(1, tosm);
+    tos  += tosm;
     // -------------------------------------------------------------------------
 
     // go around the loop again, but
