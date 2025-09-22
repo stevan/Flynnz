a66e5dbc77add2ca24dc2cd11ecda534762fb6a2
Stevan Little <stevan.little@gmail.com>
2025-09-22 22:14:44 +0200

    fooo

---
+++ tests/100-playground.test.ts
@@ -0,0 +1,30 @@
+
+import * as Debugger from '../src/Debugger.js'
+import * as Machine  from '../src/Machine.js'
+
+import {
+    SCAN, JUMP, HALT, ERR,
+    PUSH, DUP, POP, SWAP, ROT,
+    NEG, ADD, SUB, MUL, DIV, MOD,
+    EQ, NE, LT, LE, GT, GE,
+    NOT, AND, OR,
+    EQZ, ANY,
+    ___, TRUE, FALSE,
+} from '../src/ISA.js'
+
+export const countdown = [ // result should be 10 -> 1 range
+    [ SCAN,  PUSH,  10,  1, TRUE,   ],
+    [ SCAN,  DUP,  ___,  1, FALSE,  ],
+    [ SCAN,  PUSH,   1,  1, FALSE,  ],
+    [ SCAN,  SUB,  ___,  1, TRUE,   ],
+    [ SCAN,  DUP,  ___,  1, FALSE,  ],
+    [ SCAN,  PUSH,   1,  1, FALSE,  ],
+    [ SCAN,  EQ,   ___,  1, FALSE,  ],
+    [ JUMP,  EQZ,  ___, -6, FALSE,  ],
+    [ HALT,  ___,  ___,  0, FALSE,  ],
+];
+
+
+Debugger.runPrograms(Machine, [
+    [ 'countdown', countdown ],
+]);
