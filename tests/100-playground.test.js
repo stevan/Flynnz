e9a5f4c42e9815850c38cb304d4fe46e1a94ba7a
Stevan Little <stevan.little@gmail.com>
2025-09-23 14:55:42 +0200

    cleanup

--- tests/100-playground.test.js
+++ tests/100-playground.test.js
@@ -14,21 +14,20 @@ import {
 } from '../src/ISA.js'
 
 export const countdown = [
-    [ COMM,  GET,  ___,  1, FALSE,  ],
-    [ COMM,  PUT,  ___,  1, FALSE,  ],
-    [ SCAN,  DUP,  ___,  1, FALSE,  ],
-    [ SCAN,  PUSH,   1,  1, FALSE,  ],
-    [ SCAN,  SUB,  ___,  1, FALSE,  ],
-    [ COMM,  PUT,  ___,  1, FALSE,  ],
-    [ SCAN,  DUP,  ___,  1, FALSE,  ],
-    [ SCAN,  PUSH,   1,  1, FALSE,  ],
-    [ SCAN,  EQ,   ___,  1, FALSE,  ],
-    [ JUMP,  EQZ,  ___, -6, FALSE,  ],
-    [ HALT,  ___,  ___,  0, FALSE,  ],
+    [ COMM,  GET,  ___,  1, TRUE  ],
+    [ COMM,  PUT,  ___,  1, FALSE ],
+    [ SCAN,  DUP,  ___,  1, FALSE ],
+    [ SCAN,  PUSH,   1,  1, FALSE ],
+    [ SCAN,  SUB,  ___,  1, TRUE  ],
+    [ COMM,  PUT,  ___,  1, FALSE ],
+    [ SCAN,  DUP,  ___,  1, FALSE ],
+    [ SCAN,  PUSH,   1,  1, FALSE ],
+    [ SCAN,  EQ,   ___,  1, FALSE ],
+    [ JUMP,  EQZ,  ___, -6, FALSE ],
+    [ HALT,  ___,  ___,  0, FALSE ],
 ];
 
 Debugger.runPrograms(Machine.load, [
     [ 'countdown', countdown, [ 10 ], [] ],
 ]);
-
 
